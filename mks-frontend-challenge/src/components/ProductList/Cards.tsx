fa2bb1eb82bf3d04c23e1450990ea36a9396ea8a
João Pedro Granelli <104399325+joaopedrogranelli@users.noreply.github.com>
2024-04-12 19:43:21 -0300

    wip

--- mks-frontend-challenge/src/components/ProductList/Cards.tsx
+++ mks-frontend-challenge/src/components/ProductList/Cards.tsx
@@ -19,19 +19,17 @@ export function Card({ product, onAddToModal }: CardProps) {
     <div className="flex flex-wrap justify-center items-center">
       <div className="flex flex-col items-center justify-center w-[217.56px] shadow-lg shimmer">
         <img
-          className="w-[111px] h-[138px]"
+          className="w-[111px] h-[138px] object-contain"
           src={product.photo}
           alt={product.name}
         />
 
-        <div className="flex h-[38px] flex-row items-center justify-center gap-1">
-          <h2 className="w-[124px] font-normal text-base leading-4">
-            {product.name}
-          </h2>
+        <div className="flex h-[38px] w-full px-3 flex-row items-center justify-between gap-1">
+          <h2 className="font-normal text-base leading-4">{product.name}</h2>
 
           <div className="flex flex-row items-center justify-center">
-            <span className=" bg-darkGray text-white font-bold text-base rounded-md">
-              R$ {product.price}
+            <span className="px-1.5 py-1 bg-darkGray text-white font-bold text-base rounded-md">
+              {"R$" + product.price}
             </span>
           </div>
         </div>
